5aa11cbf1c232e9644bd117d938da0650465b3e9
Adam Łangowski <adam.langowski@talkinthings.com>
2021-03-05 17:02:08 +0100

    Added conditional rendering and console.loging clicked checkbox

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -2,12 +2,24 @@ import React, { Component } from 'react'
 import { Checkbox } from "antd"
 
 export default class User extends Component {
+
+    state = {
+        checked: false
+    }
+
+    checkCheckbox() {
+        console.log(this.props.id)
+        this.setState({
+            checked: !this.state.checked
+        })
+    }
+
     render() {
         return (
             <tr style={{ width: "100%" }}>
                 <td><img src={this.props.avatar} alt="" width={23} /></td>
                 <td>{this.props.name}</td>
-                <td><Checkbox onClick={console.log(this.props.id)} /></td>
+                <td><Checkbox checked={this.state.checked} onClick={console.log(this.props.id)} /></td>
             </tr>
         )
     }
