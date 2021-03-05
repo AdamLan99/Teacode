0688e7e97d1a688a47acf1134546ff67c0f4d03b
Adam Łangowski <adam.langowski@talkinthings.com>
2021-03-05 16:07:11 +0100

    Little styling change

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react'
 import User from './User'
 //import "rsuite/dist/styles/rsuite-default.css";
-import { Pagination, Input } from "antd"
+import { Input } from "antd"
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 const { Search } = Input
